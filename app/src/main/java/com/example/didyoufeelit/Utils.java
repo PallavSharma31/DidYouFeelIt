3b7dacd0462798c236a64049356aa9afd70c0308
PallavSharma31 <pallavsharma2727@gmail.com>
2022-01-26 18:40:03 +0530

    this is our third commt

--- app/src/main/java/com/example/didyoufeelit/Utils.java
+++ app/src/main/java/com/example/didyoufeelit/Utils.java
@@ -19,6 +19,7 @@ import java.nio.charset.Charset;
 public final class Utils {
 
     public static int TIME_PASS=5;
+    this is written by me intentionally to learn git
     /** Tag for the log messages */
     public static final String LOG_TAG = Utils.class.getSimpleName();
 
